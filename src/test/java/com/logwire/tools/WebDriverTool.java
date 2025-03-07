e5e5fe30aaa040b509488170b0c442885a5ddc71
AchourSimoud <achoursimoud@gmail.com>
2025-03-08 00:03:23 +0100

    jenkinsfile

--- src/test/java/com/logwire/tools/WebDriverTool.java
+++ src/test/java/com/logwire/tools/WebDriverTool.java
@@ -3,6 +3,7 @@ package com.logwire.tools;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions; // Import ChromeOptions
 
 public class WebDriverTool {
 
